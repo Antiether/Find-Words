d5e9c3f12475c18b885c980dbdf977bf7ac73f62
Kemas_M_Alfath <153353493+kemasmalfath@users.noreply.github.com>
2024-04-28 09:20:25 +0700

    Update Find Words.cpp
    membuat looping untuk mengecek kata secara vertikal dan horizontal

--- Find Words.cpp
+++ Find Words.cpp
@@ -1,3 +1,51 @@
 #include <iostream>
 #include <cstring>
 using namespace std;
+
+
+
+
+
+int kataLen = strlen(word);
+
+//mengecek secara vertikal
+  for (int i = 0; i <= 24; i++) 
+	{
+        for (int j = 0; j < 24; j++) 
+		{
+            bool found = true;
+            for (int k = 0; k < kataLen; k++) 
+			{
+                if (data_matriks[i + k][j] != word[k]) 
+				{
+                    found = false;
+                    break;
+                }
+            }
+            if (found) 
+			{
+                return true;
+            }
+        }
+    }
+
+ // menegcek secara horizontal
+  for (int i = 0; i < 24; i++) 
+	{ 
+       for (int j = 0; j <= 24 - kataLen; j++) 
+	   { 
+          bool found = true;
+          for (int k = 0; k < kataLen; k++) 
+		  { 
+               if (data_matriks[i][j + k] != word[k]) 
+			   {
+                found = false;
+                break; 
+           }
+        }
+        if (found) 
+		{
+            return true; 
+           }
+       }
+    }
