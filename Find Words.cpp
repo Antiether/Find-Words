bfaba880925d0196b9a0301a628618660e25f7a3
Adila Nurul <153069365+adilanh@users.noreply.github.com>
2024-04-28 11:55:47 +0700

    Update Find Words.cpp
    Mengubah Data Menjadi Huruf Kapital

--- Find Words.cpp
+++ Find Words.cpp
@@ -2,8 +2,43 @@
 #include <cstring>
 using namespace std;
 
+//make data be a capital
+void kapital (char* str) 
+{
+    for (int i = 0; str[i] != '\0'; i++) 
+	{
+        str[i] = toupper(str[i]);
+    }
+}
 
 
+bool cek_kata(const char* word) 
+{
+    char data_matriks[24][24] = {
+        "AAFLKHPFSSUFICICLESGNNH",
+        "SFVREOMRWLRTTSXOQQNAOAO",
+        "QEIAIFXAEIRFVFYSXIMINJI",
+        "WSTRLGOCAPBIAFIWIWTUACM",
+        "FEYAEAISTPCRLUJKOAKCERS",
+        "RVDAKPNDEEHDEMSNCKKFOAH",
+        "MRNEDSLCRRIWNRSAAFITMMI",
+        "YAAECIEAHYMOTAVHRSSTISB",
+        "RJSEWELCCENNIETOHWSGLSE",
+        "ATANYYMOIEESNESIOIRELTR",
+        "UTENEWEBHMYBETNNRAIEBEN",
+        "RCLKUTEAEQJLSGSHTGDSKOA",
+        "BHOICATNRRSDDECEHOOLGIT",
+        "ENSLUARIRSETALOCOHCTOHE",
+        "FZFUDQJYMADOYIWYGLOVESU",
+        "TEKALFWONSNAEBMIEJTZNTG",
+        "ESWPOSJXEUTUYOZUWAKEZHM",
+        "KZUHBPEZEERFLMSNOWBALLH",
+        "NSNOWBOARDYTVWYCLEVOHSA",
+        "ACOCRQLGZIYCHODRAZZILBI",
+        "LBVKKWANZAAQINWOLPWONSL",
+        "BFREEZINGRAINSLILGTMELT",
+        "HQPYLWHFMNFFUFPSWXNUMMV"
+    };
 
 
 int kataLen = strlen(word);
